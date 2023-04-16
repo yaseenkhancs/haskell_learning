453401c306aee1fda1ec983682612144fc64ad81
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2023-04-16 22:48:01 +0100

    ex 2b 3a, 3b,

---
+++ Ex_threeB.hs
@@ -0,0 +1,193 @@
+module Ex_threeB where
+
+------------------------------------------------------------------------------
+-- Ex 1: given numbers start, count and end, build a list that starts
+-- with count copies of start and ends with end.
+--
+-- Use recursion and the : operator to build the list.
+--
+-- Examples:
+--   buildList 1 5 2 ==> [1,1,1,1,1,2]
+--   buildList 7 0 3 ==> [3]
+
+buildList :: Int -> Int -> Int -> [Int]
+buildList _ 0 end = [end]
+buildList start count end = start : buildList start (count-1) end
+
+------------------------------------------------------------------------------
+-- Ex 2: given i, build the list of sums [1, 1+2, 1+2+3, .., 1+2+..+i]
+--
+-- Use recursion and the : operator to build the list.
+--
+-- Ps. you'll probably need a recursive helper function
+
+sums :: Int -> [Int]
+sums i = sums' 1 i
+
+sums' i max
+  | i == max = [sumnum i]
+  | otherwise = sumnum i : sums' (i+1) max
+
+sumnum 0 = 0
+sumnum i = i + sumnum(i-1)
+
+------------------------------------------------------------------------------
+-- Ex 3: define a function mylast that returns the last value of the
+-- given list. For an empty list, a provided default value is
+-- returned.
+--
+-- Use only pattern matching and recursion (and the list constructors : and [])
+--
+-- Examples:
+--   mylast 0 [] ==> 0
+--   mylast 0 [1,2,3] ==> 3
+
+mylast :: a -> [a] -> a
+mylast def [] = def
+mylast def (x:[]) = x
+mylast def (x:xs) = mylast def (xs)
+
+------------------------------------------------------------------------------
+-- Ex 4: safe list indexing. Define a function indexDefault so that
+--   indexDefault xs i def
+-- gets the element at index i in the list xs. If i is not a valid
+-- index, def is returned.
+--
+-- Use only pattern matching and recursion (and the list constructors : and [])
+--
+-- Examples:
+--   indexDefault [True] 1 False         ==>  False
+--   indexDefault [10,20,30] 0 7         ==>  10
+--   indexDefault [10,20,30] 2 7         ==>  30
+--   indexDefault [10,20,30] 3 7         ==>  7
+--   indexDefault ["a","b","c"] (-1) "d" ==> "d"
+
+indexDefault :: [a] -> Int -> a -> a
+indexDefault (x:xs) 0 def = x
+indexDefault [] i def = def
+indexDefault (x:xs) i def
+  | i > 0 = indexDefault xs (i-1) def
+
+------------------------------------------------------------------------------
+-- Ex 5: define a function that checks if the given list is in
+-- increasing order.
+--
+-- Use pattern matching and recursion to iterate through the list.
+--
+-- Examples:
+--   sorted [1,2,3] ==> True
+--   sorted []      ==> True
+--   sorted [2,7,7] ==> True
+--   sorted [1,3,2] ==> False
+--   sorted [7,2,7] ==> False
+
+sorted :: [Int] -> Bool
+sorted [] = True
+sorted (x:[]) = True
+sorted (x:y:zs)
+  | x <= y = sorted (y:zs)
+  | otherwise = False
+
+------------------------------------------------------------------------------
+-- Ex 6: compute the partial sums of the given list like this:
+--
+--   sumsOf [a,b,c]  ==>  [a,a+b,a+b+c]
+--   sumsOf [a,b]    ==>  [a,a+b]
+--   sumsOf []       ==>  []
+--
+-- Use pattern matching and recursion (and the list constructors : and [])
+
+sumsOf :: [Int] -> [Int]
+sumsOf xs = sumsOf' xs 0
+
+sumsOf' [] conc = []
+sumsOf' (x:xs) conc = (conc+x) : sumsOf' xs (conc+x)
+
+------------------------------------------------------------------------------
+-- Ex 7: implement the function merge that merges two sorted lists of
+-- Ints into a sorted list
+--
+-- Use only pattern matching and recursion (and the list constructors : and [])
+--
+-- Examples:
+--   merge [1,3,5] [2,4,6] ==> [1,2,3,4,5,6]
+--   merge [1,1,6] [1,2]   ==> [1,1,1,2,6]
+
+merge :: [Int] -> [Int] -> [Int]
+merge [] [] = []
+merge x [] = x
+merge [] x = x
+merge (x:xs) (y:ys)
+  | x <= y = x : merge xs (y:ys)
+  | otherwise = y : merge (x:xs) ys
+
+
+------------------------------------------------------------------------------
+-- Ex 8: compute the biggest element, using a comparison function
+-- passed as an argument.
+--
+-- That is, implement the function mymaximum that takes
+--
+-- * a function `bigger` :: a -> a -> Bool
+-- * a value `initial` of type a
+-- * a list `xs` of values of type a
+--
+-- and returns the biggest value it sees, considering both `initial`
+-- and all element in `xs`.
+--
+-- Examples:
+--   mymaximum (>) 3 [] ==> 3
+--   mymaximum (>) 0 [1,3,2] ==> 3
+--   mymaximum (>) 4 [1,3,2] ==> 4    -- initial value was biggest
+--   mymaximum (<) 4 [1,3,2] ==> 1    -- note changed biggerThan
+--   mymaximum (\(a,b) (c,d) -> b > d) ("",0) [("Banana",7),("Mouse",8)]
+--     ==> ("Mouse",8)
+
+mymaximum :: (a -> a -> Bool) -> a -> [a] -> a
+mymaximum bigger initial [] = initial
+mymaximum bigger initial (x:xs)
+  | bigger initial x = mymaximum bigger initial xs
+  | otherwise = mymaximum bigger x xs
+
+------------------------------------------------------------------------------
+-- Ex 9: define a version of map that takes a two-argument function
+-- and two lists. Example:
+--
+--   map2 f [x,y,z,w] [a,b,c]  ==> [f x a, f y b, f z c]
+--
+-- If the lists have differing lengths, ignore the trailing elements
+-- of the longer list.
+--
+-- Use recursion and pattern matching. Do not use any library functions.
+
+map2 :: (a -> b -> c) -> [a] -> [b] -> [c]
+map2 f [] _ = []
+map2 f _ [] = []
+map2 f (x:xs) (y:ys) = f x y : map2 f xs ys
+
+------------------------------------------------------------------------------
+-- Ex 10: implement the function maybeMap, which works a bit like a
+-- combined map & filter.
+---
+-- maybeMap is given a list ([a]) and a function of type a -> Maybe b.
+-- This function is called for all values in the list. If the function
+-- returns Just x, x will be in the result list. If the function
+-- returns Nothing, no value gets added to the result list.
+--
+-- Examples:
+--
+-- let f x = if x>0 then Just (2*x) else Nothing
+-- in maybeMap f [0,1,-1,4,-2,2]
+--   ==> [2,8,4]
+--
+-- maybeMap Just [1,2,3]
+--   ==> [1,2,3]
+--
+-- maybeMap (\x -> Nothing) [1,2,3]
+--   ==> []
+
+maybeMap :: (a -> Maybe b) -> [a] -> [b]
+maybeMap f [] = []
+maybeMap f (x:xs) = case f x of 
+  (Just c) -> c : maybeMap f (xs)
+  Nothing -> maybeMap f (xs)
---
+++ Ex_twoB.hs
@@ -0,0 +1,147 @@
+module Ex_twoB where
+
+import Data.List
+--import Data.Char
+
+------------------------------------------------------------------------------
+-- Ex 1: compute binomial coefficients using recursion. Binomial
+-- coefficients are defined by the following equations:
+--
+--   B(n,k) = B(n-1,k) + B(n-1,k-1)
+--   B(n,0) = 1
+--   B(0,k) = 0, when k>0
+--
+-- Hint! pattern matching is your friend.
+
+binomial :: Integer -> Integer -> Integer
+binomial n 0 = 1
+binomial n k
+  | n == 0 && k > 0 = 0
+  | otherwise = binomial(n-1) (k) + binomial(n-1) (k-1)
+
+------------------------------------------------------------------------------
+-- Ex 2: implement the odd factorial function. Odd factorial is like
+-- factorial, but it only multiplies odd numbers.
+--
+-- Examples:
+--   oddFactorial 7 ==> 7*5*3*1 ==> 105
+--   oddFactorial 6 ==> 5*3*1 ==> 15
+
+oddFactorial :: Integer -> Integer
+oddFactorial 1 = 1
+oddFactorial x
+  | odd x = x * oddFactorial(x-2)
+  | otherwise = oddFactorial (x-1)
+
+------------------------------------------------------------------------------
+-- Ex 3: implement the Euclidean Algorithm for finding the greatest
+-- common divisor:
+--
+-- Given two numbers, a and b,
+-- * if one is zero, return the other number
+-- * if not, subtract the smaller number from the larger one
+-- * replace the larger number with this new number
+-- * repeat
+--
+-- For example,
+--   myGcd 9 12 ==> 3
+-- In this case, the algorithm proceeds like this
+--
+--   a      b
+--
+--   9      12
+--   9      (12-9)
+--   9      3
+--   (9-3)  3
+--   6      3
+--   (6-3)  3
+--   3      3
+--   (3-3)  3
+--   0      3
+--
+-- Background reading:
+-- * https://en.wikipedia.org/wiki/Euclidean_algorithm
+
+myGcd :: Integer -> Integer -> Integer
+myGcd 0 y = y
+myGcd x 0 = x
+myGcd x y = if x > y then myGcd (x-y) y else myGcd x (y-x)
+
+------------------------------------------------------------------------------
+-- Ex 4: Implement the function leftpad which adds space characters
+-- to the start of the string until it is long enough.
+--
+-- Examples:
+--   leftpad "foo" 5 ==> "  foo"
+--   leftpad "13" 3 ==> " 13"
+--   leftpad "xxxxx" 3 ==> "xxxxx"
+--
+-- Tips:
+-- * you can combine strings with the ++ operator.
+-- * you can compute the length of a string with the length function
+
+leftpad :: String -> Int -> String
+leftpad s i = if i > length s then leftpad (" " ++ s) i else s
+
+------------------------------------------------------------------------------
+-- Ex 5: let's make a countdown for a rocket! Given a number, you
+-- should produce a string that says "Ready!", counts down from the
+-- number, and then says "Liftoff!".
+--
+-- For example,
+--   countdown 4 ==> "Ready! 4... 3... 2... 1... Liftoff!"
+--
+-- Hints:
+-- * you can combine strings with the ++ operator
+-- * you can use the show function to convert a number into a string
+-- * you'll probably need a recursive helper function
+
+countdown :: Integer -> String
+countdown i = "Ready! " ++ cthelper i ++ "Liftoff!"
+
+cthelper 0 = ""
+cthelper i = show i ++ "... " ++ cthelper (i-1)
+
+------------------------------------------------------------------------------
+-- Ex 6: implement the function smallestDivisor that returns the
+-- smallest number (greater than 1) that divides the given number evenly.
+--
+-- That is, when
+--   smallestDivisor n ==> k
+-- we have
+--   n = t*k
+-- for some t.
+--
+-- Ps. your function doesn't need to work for inputs 0 and 1, but
+-- remember this in the next exercise!
+--
+-- Hint: remember the mod function!
+
+smallestDivisor :: Integer -> Integer
+smallestDivisor n = smalldivhelper n (n-1)
+  where smalldivhelper x y = if x `mod` y == 0 then y else smalldivhelper x (y-1)
+--smalldivhelper :: Integer -> Integer -> Integer
+
+------------------------------------------------------------------------------
+-- Ex 7: implement a function isPrime that checks if the given number
+-- is a prime number. Use the function smallestDivisor.
+--
+-- Ps. 0 and 1 are not prime numbers
+
+isPrime :: Integer -> Bool
+isPrime x = if smallestDivisor x == 1 then True else False
+
+------------------------------------------------------------------------------
+-- Ex 8: implement a function biggestPrimeAtMost that returns the
+-- biggest prime number that is less than or equal to the given
+-- number. Use the function isPrime you just defined.
+--
+-- You don't need to care about arguments less than 2. Any behaviour
+-- for them is fine.
+--
+-- Examples:
+--   biggestPrimeAtMost 3 ==> 3
+--   biggestPrimeAtMost 10 ==> 7
+
+biggestPrimeAtMost :: Integer -> Integer
+biggestPrimeAtMost x = if isPrime x then x else biggestPrimeAtMost(x-1)
